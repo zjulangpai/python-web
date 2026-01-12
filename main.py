6efacc5c444b39fd283e4ce5560ed86bc549f93e
zjulangpai <zjulangpai@163.com>
2026-01-12 14:30:09 +0800

    test build

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = FastAPI()
 
 @app.get("/health")
 def health():
-    return {"status": "ok1"}
+    return {"status": "ok2"}
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
